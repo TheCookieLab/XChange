8de55195ebee727962a776a8f7c7181a1687b8cb
Dmitri Karpovich <dmitri.karpovich@gmail.com>
2024-08-26 23:38:40 +0200

    [bitfinex] Add integration tests

---
+++ xchange-bitfinex/src/test/java/org/knowm/xchange/bitfinex/service/BitfinexMarketDataServiceIntegration.java
@@ -0,0 +1,81 @@
+package org.knowm.xchange.bitfinex.service;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.io.IOException;
+import java.util.List;
+import org.junit.jupiter.api.Test;
+import org.knowm.xchange.ExchangeFactory;
+import org.knowm.xchange.bitfinex.BitfinexExchange;
+import org.knowm.xchange.currency.CurrencyPair;
+import org.knowm.xchange.dto.Order.OrderType;
+import org.knowm.xchange.dto.marketdata.OrderBook;
+import org.knowm.xchange.dto.marketdata.Ticker;
+import org.knowm.xchange.dto.meta.ExchangeHealth;
+
+public class BitfinexMarketDataServiceIntegration {
+
+  BitfinexExchange exchange = ExchangeFactory.INSTANCE.createExchange(BitfinexExchange.class);
+
+  @Test
+  public void exchange_health() {
+    assertThat(exchange.getMarketDataService().getExchangeHealth()).isEqualTo(ExchangeHealth.ONLINE);
+  }
+
+
+  @Test
+  public void valid_tickers() throws IOException {
+    List<Ticker> tickers = exchange.getMarketDataService().getTickers(null);
+    assertThat(tickers).isNotEmpty();
+
+    assertThat(tickers).allSatisfy(ticker -> {
+      assertThat(ticker.getInstrument()).isNotNull();
+      assertThat(ticker.getLast()).isNotNegative();
+
+      assertThat(ticker.getBidSize()).isNotNegative();
+      assertThat(ticker.getAskSize()).isNotNegative();
+
+      assertThat(ticker.getAsk()).isNotNegative();
+      assertThat(ticker.getBid()).isNotNegative();
+      assertThat(ticker.getBid()).isLessThanOrEqualTo(ticker.getAsk());
+    });
+  }
+
+
+  @Test
+  void valid_single_ticker() throws IOException {
+    Ticker ticker = exchange.getMarketDataService().getTicker(CurrencyPair.BTC_USDT);
+
+    assertThat(ticker.getInstrument()).isEqualTo(CurrencyPair.BTC_USDT);
+    assertThat(ticker.getLast()).isNotNull();
+
+    if (ticker.getBid().signum() > 0 && ticker.getAsk().signum() > 0) {
+      assertThat(ticker.getBid()).isLessThan(ticker.getAsk());
+    }
+
+  }
+
+
+  @Test
+  void valid_orderbook() throws IOException {
+    OrderBook orderBook = exchange.getMarketDataService().getOrderBook(CurrencyPair.BTC_USDT);
+
+    assertThat(orderBook.getBids()).isNotEmpty();
+    assertThat(orderBook.getAsks()).isNotEmpty();
+
+    assertThat(orderBook.getAsks().get(0).getLimitPrice()).isGreaterThan(orderBook.getBids().get(0).getLimitPrice());
+
+    assertThat(orderBook.getBids()).allSatisfy(limitOrder -> {
+      assertThat(limitOrder.getInstrument()).isEqualTo(CurrencyPair.BTC_USDT);
+      assertThat(limitOrder.getType()).isEqualTo(OrderType.BID);
+    });
+
+    assertThat(orderBook.getAsks()).allSatisfy(limitOrder -> {
+      assertThat(limitOrder.getInstrument()).isEqualTo(CurrencyPair.BTC_USDT);
+      assertThat(limitOrder.getType()).isEqualTo(OrderType.ASK);
+    });
+
+  }
+
+
+}
