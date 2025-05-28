4f6b7ba60e80b484563c4973bbdcd9cd11e72470
kevin tan <kevintanhongann@gmail.com>
2025-05-28 22:39:23 +0800

    [tradeogre] Implement trade history retrieval and response handling in TradeOgre service

--- xchange-tradeogre/src/main/java/org/knowm/xchange/tradeogre/service/TradeOgreTradeService.java
+++ xchange-tradeogre/src/main/java/org/knowm/xchange/tradeogre/service/TradeOgreTradeService.java
@@ -5,10 +5,15 @@ import java.util.List;
 import java.util.stream.Collectors;
 import org.knowm.xchange.dto.trade.LimitOrder;
 import org.knowm.xchange.dto.trade.OpenOrders;
+import org.knowm.xchange.dto.trade.UserTrades;
 import org.knowm.xchange.service.trade.TradeService;
+import org.knowm.xchange.service.trade.params.TradeHistoryParams;
+import org.knowm.xchange.service.trade.params.orders.DefaultOpenOrdersParam;
 import org.knowm.xchange.service.trade.params.orders.OpenOrdersParams;
 import org.knowm.xchange.tradeogre.TradeOgreAdapters;
 import org.knowm.xchange.tradeogre.TradeOgreExchange;
+import org.knowm.xchange.tradeogre.dto.trade.TradeOgreOrdersResponse;
+import org.knowm.xchange.tradeogre.dto.trade.TradeOgreTrade;
 
 public class TradeOgreTradeService extends TradeOgreTradeServiceRaw implements TradeService {
 
@@ -23,15 +28,27 @@ public class TradeOgreTradeService extends TradeOgreTradeServiceRaw implements T
 
   @Override
   public OpenOrders getOpenOrders() throws IOException {
-    return getOpenOrders(null);
+    return getOpenOrders(createOpenOrdersParams());
   }
 
   @Override
   public OpenOrders getOpenOrders(OpenOrdersParams params) throws IOException {
+    TradeOgreOrdersResponse ordersResponse = getOrders();
     List<LimitOrder> orders =
-        TradeOgreAdapters.adaptOpenOrders(getOrders()).getOpenOrders().stream()
+        TradeOgreAdapters.adaptOpenOrders(ordersResponse.getOrders()).getOpenOrders().stream()
             .filter(params != null ? params::accept : o -> true)
             .collect(Collectors.toList());
     return new OpenOrders(orders);
+  }
+
+  @Override
+  public OpenOrdersParams createOpenOrdersParams() {
+    return new DefaultOpenOrdersParam();
+  }
+
+  @Override
+  public UserTrades getTradeHistory(TradeHistoryParams params) throws IOException {
+    List<TradeOgreTrade> tradeHistory = getTradeHistory();
+    return TradeOgreAdapters.adaptTradeHistory(tradeHistory);
   }
 }
