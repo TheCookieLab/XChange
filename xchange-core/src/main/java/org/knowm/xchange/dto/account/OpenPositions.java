285a2fecf6b5943516a8b2895742b4290855d5c2
Dmitri Karpovich <dmitri.karpovich@gmail.com>
2025-09-09 00:33:16 +0200

    [general] Add constructor

--- xchange-core/src/main/java/org/knowm/xchange/dto/account/OpenPositions.java
+++ xchange-core/src/main/java/org/knowm/xchange/dto/account/OpenPositions.java
@@ -5,10 +5,12 @@ import java.util.List;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
 @Builder
 @AllArgsConstructor
+@NoArgsConstructor
 public class OpenPositions implements Serializable {
 
   private List<OpenPosition> openPositions;
