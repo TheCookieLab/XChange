f3c21a329de3f68094632ffca610a5d5fe1623bb
Dmitri Karpovich <dmitri.karpovich@gmail.com>
2025-09-12 00:25:07 +0200

    [stream-kraken] Extract sample messages to files

--- xchange-stream-kraken-v2/src/test/java/info/bitrich/xchangestream/kraken/dto/response/KrakenMessageTest.java
+++ xchange-stream-kraken-v2/src/test/java/info/bitrich/xchangestream/kraken/dto/response/KrakenMessageTest.java
@@ -10,17 +10,16 @@ class KrakenMessageTest {
 
   @Test
   void render() throws Exception {
-    var status = "{\"channel\":\"status\",\"type\":\"update\",\"data\":[{\"version\":\"2.0.10\",\"system\":\"online\",\"api_version\":\"v2\",\"connection_id\":10686724941062004662}]}";
-    var method = "{\"method\":\"subscribe\",\"result\":{\"channel\":\"trade\",\"snapshot\":true,\"symbol\":\"BTC/USDT\"},\"success\":true,\"time_in\":\"2025-09-09T21:31:48.558238Z\",\"time_out\":\"2025-09-09T21:31:48.558280Z\"}";
-    var hb = "{\"channel\":\"heartbeat\"}";
-    var trade = "{\"channel\":\"trade\",\"type\":\"update\",\"data\":[{\"symbol\":\"BTC/USD\",\"side\":\"buy\",\"price\":111399.1,\"qty\":0.00037711,\"ord_type\":\"limit\",\"trade_id\":86881079,\"timestamp\":\"2025-09-09T21:31:50.716224Z\"}]}";
-
     ObjectMapper om = Config.getInstance().getObjectMapper();
 
-    assertThat(om.readValue(status, KrakenMessage.class)).isInstanceOf(KrakenStatusMessage.class);
-    assertThat(om.readValue(method, KrakenMessage.class)).isInstanceOf(KrakenControlMessage.class);
-    assertThat(om.readValue(hb, KrakenMessage.class)).isInstanceOf(KrakenHeartbeatMessage.class);
-    assertThat(om.readValue(trade, KrakenMessage.class)).isInstanceOf(KrakenTradeMessage.class);
+    assertThat(om.readValue(getClass().getResource("/sample-messages/status.json"), KrakenMessage.class))
+        .isInstanceOf(KrakenStatusMessage.class);
+    assertThat(om.readValue(getClass().getResource("/sample-messages/method.json"), KrakenMessage.class))
+        .isInstanceOf(KrakenControlMessage.class);
+    assertThat(om.readValue(getClass().getResource("/sample-messages/heartbeat.json"), KrakenMessage.class))
+        .isInstanceOf(KrakenHeartbeatMessage.class);
+    assertThat(om.readValue(getClass().getResource("/sample-messages/trade.json"), KrakenMessage.class))
+        .isInstanceOf(KrakenTradeMessage.class);
   }
 
 }
