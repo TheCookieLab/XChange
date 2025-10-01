d6d03f2404385a84a5d087b779abe19e24fe2a24
bidlako <bi.ondrej@gmail>
2025-10-01 08:38:26 +0200

    chore(coinmate): deprecate stop-loss parameters

--- xchange-coinmate/src/main/java/org/knowm/xchange/coinmate/CoinmateAuthenticated.java
+++ xchange-coinmate/src/main/java/org/knowm/xchange/coinmate/CoinmateAuthenticated.java
@@ -150,6 +150,12 @@ public interface CoinmateAuthenticated extends Coinmate {
       @FormParam("orderId") String orderId)
       throws IOException;
 
+  /**
+   * Creates a limit buy order.
+   *
+   * @param stopPrice DEPRECATED: Disabled by Coinmate API as of 2025-02-27.
+   * @param trailing DEPRECATED: Disabled by Coinmate API as of 2025-02-27.
+   */
   @POST
   @Path("buyLimit")
   CoinmateTradeResponse buyLimit(
@@ -160,14 +166,20 @@ public interface CoinmateAuthenticated extends Coinmate {
       @FormParam("amount") BigDecimal amount,
       @FormParam("price") BigDecimal price,
       @FormParam("currencyPair") String currencyPair,
-      @FormParam("stopPrice") BigDecimal stopPrice,
+      @Deprecated @FormParam("stopPrice") BigDecimal stopPrice,
       @FormParam("hidden") Integer hidden,
       @FormParam("postOnly") Integer postOnly,
       @FormParam("immediateOrCancel") Integer immediateOrCancel,
-      @FormParam("trailing") Integer trailing,
+      @Deprecated @FormParam("trailing") Integer trailing,
       @FormParam("clientOrderId") String clientOrderId)
       throws IOException;
 
+  /**
+   * Creates a limit sell order.
+   *
+   * @param stopPrice DEPRECATED: Disabled by Coinmate API as of 2025-02-27.
+   * @param trailing DEPRECATED: Disabled by Coinmate API as of 2025-02-27.
+   */
   @POST
   @Path("sellLimit")
   CoinmateTradeResponse sellLimit(
@@ -178,11 +190,11 @@ public interface CoinmateAuthenticated extends Coinmate {
       @FormParam("amount") BigDecimal amount,
       @FormParam("price") BigDecimal price,
       @FormParam("currencyPair") String currencyPair,
-      @FormParam("stopPrice") BigDecimal stopPrice,
+      @Deprecated @FormParam("stopPrice") BigDecimal stopPrice,
       @FormParam("hidden") Integer hidden,
       @FormParam("postOnly") Integer postOnly,
       @FormParam("immediateOrCancel") Integer immediateOrCancel,
-      @FormParam("trailing") Integer trailing,
+      @Deprecated @FormParam("trailing") Integer trailing,
       @FormParam("clientOrderId") String clientOrderId)
       throws IOException;
 
@@ -498,6 +510,12 @@ public interface CoinmateAuthenticated extends Coinmate {
       @FormParam("transactionId") Long transactionId)
       throws IOException;
 
+  /**
+   * Replaces an existing order with a new limit buy order.
+   *
+   * @param stopPrice DEPRECATED: Disabled by Coinmate API as of 2025-02-27.
+   * @param trailing DEPRECATED: Disabled by Coinmate API as of 2025-02-27.
+   */
   @POST
   @Path("replaceByBuyLimit")
   CoinmateReplaceResponse replaceByBuyLimit(
@@ -509,14 +527,20 @@ public interface CoinmateAuthenticated extends Coinmate {
       @FormParam("price") BigDecimal price,
       @FormParam("currencyPair") String currencyPair,
       @FormParam("orderIdToBeReplaced") String orderIdToBeReplaced,
-      @FormParam("stopPrice") BigDecimal stopPrice,
+      @Deprecated @FormParam("stopPrice") BigDecimal stopPrice,
       @FormParam("hidden") Integer hidden,
       @FormParam("postOnly") Integer postOnly,
       @FormParam("immediateOrCancel") Integer immediateOrCancel,
-      @FormParam("trailing") Integer trailing,
+      @Deprecated @FormParam("trailing") Integer trailing,
       @FormParam("clientOrderId") String clientOrderId)
       throws IOException;
 
+  /**
+   * Replaces an existing order with a new limit sell order.
+   *
+   * @param stopPrice DEPRECATED: Disabled by Coinmate API as of 2025-02-27.
+   * @param trailing DEPRECATED: Disabled by Coinmate API as of 2025-02-27.
+   */
   @POST
   @Path("replaceBySellLimit")
   CoinmateReplaceResponse replaceBySellLimit(
@@ -528,11 +552,11 @@ public interface CoinmateAuthenticated extends Coinmate {
       @FormParam("price") BigDecimal price,
       @FormParam("currencyPair") String currencyPair,
       @FormParam("orderIdToBeReplaced") String orderIdToBeReplaced,
-      @FormParam("stopPrice") BigDecimal stopPrice,
+      @Deprecated @FormParam("stopPrice") BigDecimal stopPrice,
       @FormParam("hidden") Integer hidden,
       @FormParam("postOnly") Integer postOnly,
       @FormParam("immediateOrCancel") Integer immediateOrCancel,
-      @FormParam("trailing") Integer trailing,
+      @Deprecated @FormParam("trailing") Integer trailing,
       @FormParam("clientOrderId") String clientOrderId)
       throws IOException;
 
